fc2c2080f4369f240788a92fc0e8aa3575c770ba
“MorisLuis” <moradoluisenrique@gmail.com>
2024-10-29 15:18:51 -0600

    S / Setup page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,80 @@
+import styles from "../styles/Page.module.scss";
+import respelogos from '../public/respelogos.png';
+import trabajo from '../public/trabajo.png';
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.pageHeader}>
+        <Image
+          src={respelogos}
+          alt={'Imagen Land Page'}
+          priority
+          quality={100}
+          layout="intrinsic"
+        />
+      </div>
+
+      <div className={styles.pageTopbar}>
+        <div className={styles.pageTopbar__imagen}>
+        <Image
+          src={trabajo}
+          alt={'Imagen Land Page'}
+          priority
+          quality={100}
+          layout="intrinsic"
+        />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.pageTopbar__title}>
+          <h4>Determinación de solicitud de registro</h4>
+        </div>
+      </div>
+
+      <div className={styles.pageContent}>
+        <div className={styles.pageContent__left}>
+          <div>
+            <label htmlFor="Razón social">Razón social</label>
+            <p>MARIZA HERNÁNDEZ LUCIO</p>
+          </div>
+
+          <div>
+            <label htmlFor="Fecha de respuesta:">Fecha de respuesta:</label>
+            <p>2024-08-02</p>
+          </div>
+
+          <div>
+            <label htmlFor="Razón social">Formato:</label>
+            <p>STPS/UTD/DGIFT/ARR/17819/2024</p>
+          </div>
+        </div>
+
+        <div className={styles.pageContent__right}>
+          <h3>AVISO DE RENOVACIóN DE REGISTRO</h3>
+          <p>Fundamento de la respuesta:</p>
+          <p>DÉCIMO, DÉCIMO SEGUNDO, DÉCIMO TERCERO, DÉCIMO QUINTO-B, DÉCIMO QUINTO-C y DÉCIMO NOVENO del ACUERDO POR EL QUE SE DAN A CONOCER LAS DISPOSIONES DE CARÁCTER GENERAL PARA EL REGISTRO DE PERSONAS FÍSICAS O MORALES QUE PRESTEN SERVICIOS ESPECIALIZADOS O EJECUTEN OBRAS ESPECIALIZADAS A QUE SE REFIERE EL ARTICULO 15 DE LA LEY FEDERAL DEL TRABAJO, publicado en el Diario Oficial de la Federación el 24 de mayo de 2021y sus respectivos Acuerdos Modificatorios de fecha 03 de febrero de 2023 y 21 de febrero de 2024.</p>
+        </div>
+      </div>
+
+      <div className={styles.footer}>
+        <table>
+          <thead>
+            <tr>
+              <th>Actividad</th>
+              <th>Folio</th>
+              <th>Actividades Complementarias prestadas únicamente a empresas de su mismo grupo empresarial</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>35707033-4185-4e12-a8aa-93dd67694155</td>
+              <td>Administración y supervisión de construcción de otras obras de ingeniería civil u obra pesada</td>
+              <td>No</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
